a58c5b82819eff433a96a720fcf164bd3a8f56b4
kalyan <reddy.saikalyan90@gmail.com>
2022-06-29 22:28:38 +0530

    Added Prime Number Filter function

--- firstprogram.py
+++ firstprogram.py
@@ -9,3 +9,16 @@ for i in range(x):
     b = input()
     y.append(int(b))
 print("Sum of numbers in list is {}".format(sum_Elements_List(y)))
+
+def isPrime(d):
+    if d==1:
+        return False
+    for i in range(2,d):
+        if d%i==0:
+            return False
+    else:
+        return True
+
+print(list(filter(isPrime,y)))
+
+
